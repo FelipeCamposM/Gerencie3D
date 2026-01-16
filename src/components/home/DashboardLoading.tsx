291c021d08ba7acadec7a8f918da93ee6fa8e358
Felipe Campos Macedo <felipecamposmacedo@gmail.com>
2026-01-16 09:50:15 -0300

    fix: deleted home on components paste

--- src/components/home/DashboardLoading.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { DashboardLoading as BaseDashboardLoading } from "../LoadingPage";
-
-export const DashboardLoading: React.FC = () => {
-  return <BaseDashboardLoading />;
-};
--- src/components/home/index.ts
+++
@@ -1,8 +0,0 @@
-export { WelcomeHeader } from "./WelcomeHeader";
-export { DashboardStats } from "./DashboardStats";
-export { QuickActions } from "./QuickActions";
-export { PerformanceOverview } from "./PerformanceOverview";
-export { RecentActivity } from "./RecentActivity";
-export { TipsAndInfo } from "./TipsAndInfo";
-export { SystemStatus } from "./SystemStatus";
-export { DashboardLoading } from "./DashboardLoading";
